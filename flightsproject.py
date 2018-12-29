d4a001c69b2e19e54eed67a6e9e5cbe27b14a205
clydethefrog <clyde_the_frog@Mourads-MacBook-Pro.local>
2018-12-29 15:47:14 -0500

    first commit

--- flightsproject.py
+++ flightsproject.py
@@ -1,1 +1,46 @@
 
+
+
+#https://en.wikipedia.org/wiki/List_of_Air_Canada_destinations
+
+#airlines:
+#Air_Canada
+#Delta_Airlines
+#JetBlue
+#Southwest_Airlines
+#Spirit_Airlines (not in a table)
+#WestJet
+
+
+import pandas as pd
+
+airlines = ['Air_Canada','Delta_Air_Lines','JetBlue','Southwest_Airlines','Spirit_Airlines','WestJet']
+
+link = 'https://en.wikipedia.org/wiki/List_of_{}_destinations'.format(airlines[0])
+
+wiki_data = pd.read_html(link)
+
+destinations = wiki_data[1]
+
+country = destinations[0][2:]
+city = destinations[1][2:]
+airport = destinations[2][2:]
+
+
+
+
+
+dictionary = dict()
+for i,j in enumerate(list(country)):
+    temp_dict = dict()
+    relevant_city = list(city)[i]
+    relevant_airport = tuple([list(airport)[i]])
+    temp_dict[relevant_city] = relevant_airport
+
+    if j not in dictionary.keys():
+        dictionary[j] = tuple([temp_dict])
+    else:
+        dictionary[j] += tuple([temp_dict])
+
+
+print(dictionary)
