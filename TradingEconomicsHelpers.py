95e0149149380ccf4424516a2218750a81a01a01
clydethefrog <clyde_the_frog@Mourads-MacBook-Pro.local>
2018-12-29 17:22:52 -0500

    Added flight destinations

--- TradingEconomicsHelpers.py
+++ TradingEconomicsHelpers.py
@@ -17,21 +17,11 @@ class TradingEconomicsHelpers():
         last_rate = list(data['Last '])
         previous_rate = list(data['Previous '])
 
-        dictionary = dict()
 
-        for i,j in enumerate(country_name):
-            temp_dict = dict()
-
-            temp_dict['last'] = last_rate[i]
-            temp_dict['previous'] = previous_rate[i]
-            temp_dict['change'] = float('{:.3f}'.format((last_rate[i] - previous_rate[i]) / previous_rate[i]))
-
-            dictionary[j] = temp_dict
-
-        return dictionary
+        return {j: {'last': last_rate[i], 'previous': previous_rate[i],'change': float('{:.3f}'.format((last_rate[i] - previous_rate[i]) / previous_rate[i]))} for i, j in enumerate(country_name)}
 
 
-    
+
     #choose any link from this page >>> https://tradingeconomics.com/indicators
     # pass that link in below as a string for example:
     #link = 'https://tradingeconomics.com/country-list/tourist-arrivals'
--- flightsproject.py
+++ flightsproject.py
@@ -17,18 +17,9 @@ def function(link):
     last_rate = list(data['Last '])
     previous_rate = list(data['Previous '])
 
-    dictionary = dict()
+    return {j: {'last': last_rate[i], 'previous': previous_rate[i],'change': float('{:.3f}'.format((last_rate[i] - previous_rate[i]) / previous_rate[i]))} for i, j in enumerate(country_name)}
 
-    for i,j in enumerate(country_name):
-        temp_dict = dict()
 
-        temp_dict['last'] = last_rate[i]
-        temp_dict['previous'] = previous_rate[i]
-        temp_dict['change'] = float('{:.3f}'.format((last_rate[i] - previous_rate[i]) / previous_rate[i]))
-
-        dictionary[j] = temp_dict
-
-    return dictionary
 
 
 
