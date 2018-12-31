97c7aed71035f00c8a536de9bacd3b432ac11821
clydethefrog <clyde_the_frog@Mourads-MacBook-Pro.local>
2018-12-31 10:34:24 -0500

    comments added

--- CompanyInformation.py
+++ CompanyInformation.py
@@ -21,30 +21,40 @@ class CompanyDetails:
         response = urllib.request.urlopen(urllib.request.Request(url,None,headers))
         return pandas.read_html(response.read())
 
-
+        #Function to search the amount of aircraft in a fleet
+        #Input: Airline name: Example: Air Canada
+        #return: {Total => number}
     def search_total_no_aircraft(self, airline):
-        #returns the total number of aircraft in the fleet
         fleet = self.scraper(airline)[15]
         return {'Total':list(fleet[list(fleet.columns)[1]])[-1]}
 
+        #function to search for the average age of the entire fleet in question
+        #Input: Airline name: Example: Air Canada
+        #Return: {Average age => number}
     def search_avg_age_aircraft(self, airline):
-        #returns the average age of ENTIRE FLEET (in aggregate) {fleet average age: x years}
         fleet = self.scraper(airline)[15]
         return {'Average age':list(fleet[list(fleet.columns)[2]])[-1]}
 
+        #function that returns the count of each TYPE of aircraft
+        #Input: Airline name: Example: Air Canada
+        #Return: {plane type 1 => 5, plane type 2 => 10}
     def search_type_aircraft(self, airline):
-        #returns a count of each type of aircraft
         fleet = self.scraper(airline)[15]
         return {j:list(fleet[1])[1:][i] for i,j in enumerate(list(fleet[0])[1:])}
 
+        #function to return the average age per TYPE of aircraft
+        #Input: Airline name: Example: Air Canada
+        #Return: {aircraft type 1 => avg age, airtype type 2 => avg age}
     def search_avg_age_per_type_aircraft(self, airline):
-        #returns the average age of each aircraft TYPE {aircraft type 1: avg age, airtype type 2: avg age}
         fleet = self.scraper(airline)[15]
         return {j:list(fleet[2])[1:][i] for i,j in enumerate(list(fleet[0])[1:])}
 
-    def search_supply_chain_proportions(self, airline):
+        #function to determine the supply chain proportions
         #this counts the amount of 'boeing' and 'airbus' and 'other' made planes returns dict:
-        #{boeing:0.4, airbus:0.4, other:0.2}
+        #Input: Airline name: Example: Air Canada
+        #Returns: {boeing:0.4, airbus:0.4, other:0.2}
+    def search_supply_chain_proportions(self, airline):
+
         fleet = self.scraper(airline)[15]
         boeing_counter = 0
         airbus_counter = 0
@@ -57,6 +67,9 @@ class CompanyDetails:
                  airbus_counter / len(fleet[list(fleet.index)[0]][1:]), 'Other':(len(fleet[list
                 (fleet.index)[0]][1:]) - airbus_counter-boeing_counter) / len(fleet[list(fleet.index)[0]][1:])}
 
+        #function to determine details of the company: name, hq, callsign, sqauck value
+        #Input: Airline name: Example: Air Canada
+        #Returns: {detail1 => x, detail2 => y, detail3 => z}
     def search_company_details(self, airline):
         # this creates a tuple of the infomration [(x,y),(z,p)] then makes it into a dictionary {x:y, z:p}
         details = self.scraper(airline)
@@ -73,11 +86,18 @@ class CompanyDetails:
 
 
 
+
+
+
+
+
+
+
 if __name__ == '__main__':
     obj = CompanyDetails()
 
-    print(obj.search_supply_chain_proportions('air canada')) #british airways,
-    #  united airlines, qantas, delta air lines, spirit airlines, air france,
+    print(obj.search_supply_chain_proportions('qantas')) #british airways,
+    # united airlines, qantas, delta air lines, spirit airlines, air france,
     # american airlines, singapore airlines  etc...
 
 
@@ -111,52 +131,6 @@ if __name__ == '__main__':
 
 
 
-
-
-
-
-
-
-
-
-#this function will be used to associate company names with their respective ticker
-    #using a spreadsheet I downloaded which contains all of them
-
-##########################################################################################
-###################################### [in progress...] ##################################
-##########################################################################################
-##########################################################################################
-    #this DOES NOT work properly yet, need a better way cause a lot of these comapnies
-    #have similar names
-    def search_ticker(self, company_name):
-        data = pandas.read_csv('companies_tickers.csv')
-        tickers_list = list(data['Ticker'])
-        names_list = list(data['Name'])
-
-        dataset = list(zip(tickers_list, names_list))
-        dictionary = {}
-        for i,j in enumerate(dataset):
-            dictionary[j[1]] = j[0]
-
-        temp_dict = {}
-        for i in dictionary:
-            if '{}'.format(company_name) in str(i).lower():
-                temp_dict[i] = dictionary[i]
-
-        final_dict = dict()
-        possible_choices = list(temp_dict.items())
-        possible_choices.sort(key=lambda x: len(x[0]), reverse=True)
-
-        for i in possible_choices:
-            final_dict.update({i[0]: i[1]})
-
-        name = list(final_dict.keys())[-1]
-
-        return name
-
-
-
-#next: business and credit cycle stuff
 
 
 
